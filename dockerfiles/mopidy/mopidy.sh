a8cda41d04cc75dfb06e34bf920f3937849f85f2
Vaclav Prokes <vprokes@mouse-over.net>
2018-04-24 21:26:48 +0200

    mopidi without lastfm, spotify, but with sqlite support, better config handling

--- dockerfiles/mopidy/mopidy.sh
+++ dockerfiles/mopidy/mopidy.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-if [ -n "$SPOTIFY_USERNAME" ] && [ -n "$SPOTIFY_PASSWORD" ]; then
-    echo "Enabling Spotify..."
-    sed -i "s/\[spotify\]/[spotify]\nusername=${SPOTIFY_USERNAME}\npassword=${SPOTIFY_PASSWORD}/" /root/.config/mopidy/mopidy.conf
-else
-    echo "Disabling Spotify..."
-    sed -i 's/\[spotify\]/[spotify]\nenabled=false/' /root/.config/mopidy/mopidy.conf
-fi
-
-if [ -n "$LASTFM_USERNAME" ] && [ -n "$LASTFM_PASSWORD" ]; then
-    echo "Enabling LastFM scrobbling..."
-    sed -i "s/\[scrobbler\]/[scrobbler]\nusername=${LASTFM_USERNAME}\npassword=${LASTFM_PASSWORD}/" /root/.config/mopidy/mopidy.conf
-else
-    echo "Disabling LastFM scrobbling..."
-    sed -i 's/\[scrobbler\]/[scrobbler]\nenabled=false/' /root/.config/mopidy/mopidy.conf
-fi
+cp /root/.config/mopidy/mopidy.conf.default /root/.config/mopidy/mopidy.conf
 
 args=( )
 for i in "$@" ; do
